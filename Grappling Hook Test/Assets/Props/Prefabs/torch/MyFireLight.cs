c4f251be72f5341d0038fa6132df3aa1f719a9bd
monkesteve <shumtszching@gmail.com>
2021-04-04 18:46:23 +0800

    update level 1-5 wip 2

---
+++ Grappling Hook Test/Assets/Props/Prefabs/torch/MyFireLight.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace UnityStandardAssets.Effects
+{
+    public class MyFireLight : MonoBehaviour
+    {
+        private float m_Rnd;
+        private bool m_Burning = true;
+        private Light m_Light;
+        public int intensityFactor = 10;
+        public int minIntensity = 3;
+
+
+        private void Start()
+        {
+            m_Rnd = Random.value*100;
+            m_Light = GetComponent<Light>();
+        }
+
+
+        private void Update()
+        {
+            if (m_Burning)
+            {
+                m_Light.intensity = intensityFactor * Mathf.PerlinNoise(m_Rnd + Time.time, m_Rnd + 1 + Time.time*1) + minIntensity;
+                //float x = Mathf.PerlinNoise(m_Rnd + 0 + Time.time*2, m_Rnd + 1 + Time.time*2) - 0.5f;
+                //float y = Mathf.PerlinNoise(m_Rnd + 2 + Time.time*2, m_Rnd + 3 + Time.time*2) - 0.5f;
+                //float z = Mathf.PerlinNoise(m_Rnd + 4 + Time.time*2, m_Rnd + 5 + Time.time*2) - 0.5f;
+                //transform.localPosition = Vector3.up + new Vector3(x, y, z)*1;
+            }
+        }
+
+
+        public void Extinguish()
+        {
+            m_Burning = false;
+            m_Light.enabled = false;
+        }
+    }
+}
